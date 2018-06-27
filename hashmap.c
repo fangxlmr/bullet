04f3b43f4cb0f279f4fd6d1efba5efdf87a4f164
fangxlmr <fangxlmr@foxmail.com>
2018-06-27 20:05:49 +0800

    add good hashmap primes

--- hashmap.c
+++ hashmap.c
@@ -48,9 +48,13 @@ struct _HashMap {
 
 /*
  * 常用哈希质数表
+ * 来自：http://planetmath.org/goodhashtableprimes
  */
 static const unsigned int primes[] = {
-
+    193, 389, 769, 1543, 3079, 6151, 12289, 24593, 49157,
+    98317, 196613, 393241, 786433, 1572869, 3145739,
+    6291469, 12582917, 25165843, 50331653, 100663319,
+    201326611, 402653189, 805306457, 1610612741,
 };
 static const unsigned int primes_size
         = sizeof(primes) / sizeof(primes[0]);
