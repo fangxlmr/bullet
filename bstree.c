8d5455cd642b830da8e63ecaeee672e6a7e50a5e
fangxlmr <fangxlmr@foxmail.com>
2018-06-28 17:11:44 +0800

    code refactoring on bstree

--- bstree.c
+++ bstree.c
@@ -1,207 +1,302 @@
-#include <assert.h>
-#include "bstree.h"
+/*
+ * 二叉排序树(bstree)的代码实现
+ */
 
-#define T BSTree_T
+#include <stdlib.h>
+#include "bstree.h"
+/*
+ * 判断a, b大小，返回较大值。
+ */
 #define MAX(a, b) (((a) > (b)) ? (a) : (b))
 
-/* 结点的结构 */
-typedef struct BSTNode {
-    void *x;
-    struct BSTNode *left, *right;
-}*BSTNode;
+/*
+ * 节点结构
+ */
+typedef struct _BSTreeEntry BSTreeEntry;
 
-struct T {
-    BSTNode root;
+typedef struct _BSTreeEntry {
+    BSTreeValue data;
+    BSTreeEntry *left;
+    BSTreeEntry *right;
 };
 
+/*
+ * 二叉排序树
+ */
+struct _BSTree {
+    BSTreeEntry *root;           /* 根节点 */
+    BSTreeCompareFunc cmp;  /* 节点大小比较函数 */
+};
 
-static void delete (BSTNode node);
+BSTree *bstree_new(BSTreeCompareFunc cmp)
+{
+    BSTree *bstree;
 
-/* 新建二叉树根结点并初始化 */
-T bst_new (void) {
-    T t = (T) malloc(sizeof(struct T));
-    t->root = NULL;
-    return t;
+    bstree = (BSTree *) malloc(sizeof(BSTree));
+
+    if (!bstree) {
+        return BSTREE_NULL;
+    }
+
+    bstree->root = NULL;
+    bstree->cmp  = cmp;
+
+    return bstree;
 }
 
+/**
+ * subtree_free     销毁子树
+ *
+ * @param root      子树根节点
+ */
+static void subtree_free(BSTreeEntry *root)
+{
+    if (!root) {
+        return;
+    }
+    /*
+     * 分别销毁左右子树
+     */
+    subtree_free(root->left);
+    subtree_free(root->right);
+    /*
+     * 释放当前节点
+     */
+    free(root);
+}
 
-/* 判断二叉树是否为空 */
-int bst_empty (T root) {
-    if (!root->root)
+void bstree_free(BSTree *bstree)
+{
+    subtree_free(bstree->root);
+}
+
+int bstree_is_empty(BSTree *bstree)
+{
+    if (bstree->root == NULL) {
         return 1;
-    return 0;
+    } else {
+        return 0;
+    }
 }
 
-/* 求二叉树最大深度 */
-static int depth (BSTNode root) {
+/**
+ * subtree_depth    求子树的高度
+ *
+ * @param root      子树根节点
+ * @return          若子树为空，则饭会0，
+ *                  若子树不空，返回子树的高度。
+ */
+static int subtree_depth(BSTreeEntry *root)
+{
     if (!root)
         return 0;
-    return MAX(depth(root->left), depth(root->right)) + 1;
-}
-int bst_depth (T bst) {
-    return depth(bst->root);
+    /*
+     * 当前子树的高度为左右子树的最大值 + 1
+     */
+    return MAX(subtree_depth(root->left), subtree_depth(root->right)) + 1;
 }
 
+int bstree_depth(BSTree *bstree)
+{
+    return subtree_depth(bstree->root);
+}
 
-/* 释放二叉树 */
-static void destroy (BSTNode *root) {
-    if (!*root) {
-        return;
+/**
+ * subtree_find     查找子树中是否存在节点值等于value
+ *
+ * @param root      子树根节点
+ * @param value     待查找的元素
+ * @param cmp       节点元素比较函数
+ * @return          若子树中含有元素value，则返回非0值，
+ *                  否则返回0。
+ */
+static int subtree_find(BSTreeEntry *root,
+                        BSTreeValue value,
+                        BSTreeCompareFunc cmp)
+{
+    /* 已经遍历完毕子树的所有节点，未匹配，返回0 */
+    if (!root) {
+        return 0;
     }
-    destroy(&(*root)->left);
-    destroy(&(*root)->right);
-    free(*root);
-    *root = NULL;
-}
-void bst_free (T *bst) {
-    assert(bst && *bst);
-    destroy(&(*bst)->root);
-    *bst = NULL;
+
+    /* 当前节点即匹配 */
+    if (cmp(value, root->data) == 0) {
+        return 1;
+
+    /* 查找左子树 */
+    } else if (cmp(value, root->data) < 0) {
+        return subtree_find(root->left, value, cmp);
+
+    /* 查找有子树 */
+    } else {
+        return subtree_find(root->right, value, cmp);
+    }
 }
 
+int bstree_find(BSTree *bstree, BSTreeValue value)
+{
+    return subtree_find(bstree->root, value, bstree->cmp);
+}
 
-static int search (BSTNode root, void *key,
-                   int (*sort)(void *key1, void *key2)) {
-    if (sort(key, root->x) == 0) {
+/**
+ * subtree_add      向子树中添加元素value
+ *
+ * @param root      子树根节点的二级指针
+ * @param value     带添加的元素
+ * @param cmp       节点元素比较大小函数
+ * @return          添加成功，则返回非0值，
+ *                  添加失败（内存分配失败）则返回0。
+ */
+static int subtree_add(BSTreeEntry **root,
+                       BSTreeValue value,
+                       BSTreeCompareFunc cmp)
+{
+    BSTreeEntry *new_entry;
+
+    /*
+     * 没有匹配的节点，新建节点，并插入二叉排序树
+     */
+    if (!*root) {
+        new_entry = (BSTreeEntry *) malloc(sizeof(BSTreeEntry));
+
+        if (!new_entry) {   /* 内存分配失败 */
+            return 0;
+        }
+
+        /* 新节点 */
+        new_entry->data  = value;
+        new_entry->left  = NULL;
+        new_entry->right = NULL;
+
+        *root = new_entry;  /* 插入 */
         return 1;
     }
-    if (sort(key, root->x) < 0) {
-        return search(root->left,  key, sort);
+
+    if (cmp(value, (*root)->data) == 0) {
+        return 0;     /* 忽略重复值 */
+
+    /* 查找左子树 */
+    } else if (cmp(value, (*root)->data) < 0) {
+        subtree_add(&(*root)->left, value, cmp);
+
+    /* 查找右子树 */
     } else {
-        return search(root->right, key, sort);
+        subtree_add(&(*root)->right, value, cmp);
     }
 }
 
-/* 查找结点 */
-int bst_key_search (T bst, void *key,
-                    int (*sort)(void *key1, void *key2)) {
-    assert(bst && bst->root);
-
-    return search(bst->root, key, sort);
-}
-
-
-static void insert (BSTNode *root, void *key,
-                    int (*sort)(void *key1, void *key2)) {
-    if (!*root) {
-        BSTNode p = (BSTNode) malloc(sizeof(struct BSTNode));
-        p->x = key;
-        p->left = p->right = NULL;
-        *root = p;
-        return;
-    }
-    else if (sort(key, (*root)->x) == 0) {
-        return;     /* 忽略重复值 */
-    }
-    else if (sort(key, (*root)->x)) {
-        insert(&(*root)->left, key, sort);
-    }
-    else {
-        insert(&(*root)->right, key, sort);
-    }
-}
-
-/* 插入结点 */
-void bst_key_insert (T bst, void *key,
-                     int (*sort)(void *key1, void *key2)) {
-    assert(bst);
-    insert(&bst->root, key, sort);
+int bstree_add(BSTree *bstree, BSTreeValue value)
+{
+    return subtree_add(&bstree->root, value, bstree->cmp);
 }
 
 
 
 /**
- * 删除结点，处理待删除结点的三种情况
- * [1]  做孩子为空
- * [2]  右孩子为空
- * [3]  左右孩子都不空
+ * delete_node      删除二叉排序树中的单个节点
+ *
+ * @param node      待删除节点的二级指针
+ *
+ * @note            需要处理三种不同的节点位置情况
+ *                  [1]  左孩子为空
+ *                  [2]  右孩子为空
+ *                  [3]  左右孩子都不空
  */
-static void node_delete (BSTNode *node) {
-    BSTNode p, q;
-    if (!(*node)->left) {   /* [1] */
+static void delete_node(BSTreeEntry **node) {
+    BSTreeEntry *p, *q;
+
+    /* [1] */
+    if (!(*node)->left) {
         p = *node;
-        *node = (*node)->right;
+        *node = p->right;
         free(p);
-    } else if (!(*node)->right) {   /* [2] */
+
+     /* [2] */
+    } else if (!(*node)->right) {
         p = *node;
-        *node = (*node)->left;
+        *node = p->left;
         free(p);
-    } else {                /* [3] */
-        p = *node;
-        /*
-         * 当待删除结点的直接前驱也有左孩子
-         */
-        q = (*node)->left;
+
+    /* [3] */
+    } else {
+        p = *node;  /* 保存当前节点指针 */
+
+        /* 转左孩子，找到待删除节点的直接前驱 */
+        q = p->left;
         while (q->right) {
             p = q;
             q = q->right;
         }
-        (*node)->x = q->x;
+        /*
+         * 将直接前驱的值复制到带删除节点，
+         * 就相当于删除了待删除节点。
+         */
+        (*node)->data = q->data;
+
+        /*
+         * 处理直接前驱的子树。
+         *
+         * 如果p与*node指向相同，则重接p的左子树。
+         * 此种情况为，待删除节点的左孩子没有右子树，
+         * 直接前驱就为左孩子本身。
+         * 则将直接前驱的左孩子重接到待删除节点的左孩子处。
+         */
         if (p == *node) {
             p->left = q->left;
             free(q);
+
+        /*
+         * 如果p与*node的指向不同，则重接p的右子树。
+         * 此种情况为，待删除节点的左孩子存在右子树，
+         * 直接前驱不是左孩子。
+         * 则将直接前驱的左孩子重接到直接前驱父节点的右孩子处。
+         */
         } else {
             p->right = q->left;
             free(q);
         }
     }
 }
-/* 删除结点 */
-static void key_delete (BSTNode *root, void *key,
-                        int (*sort)(void *key1, void *key2)) {
-    if (sort(key, (*root)->x) == 0) {
-        node_delete(root);
-        return;
-    }
-    else if (sort(key, (*root)->x) < 0) {
-        key_delete(&(*root)->left, key, sort);
-    }
-    else {
-        key_delete(&(*root)->right, key, sort);
-    }
-}
-void bst_key_delete (T bst, void *key,
-                     int (*sort)(void *key1, void *key2)){
-    assert(bst);
-
-    key_delete(&bst->root, key, sort);
-}
-
-
-/*
- * TODO: 二叉树的前序、中序、后续遍历
- * TODO: 用自定义栈去实现
+/**
+ * subtree_del      删除子树中元素值等于value的节点
+ *
+ * @param root      子树根节点的二级指针
+ * @param value     目标元素值
+ * @param cmp       节点元素比较大小函数
+ * @return          删除成功，则返回非0值，
+ *                  删除失败，则返回0。
  */
-/* 二叉树的前序遍历，标准输出结果 */
-void bst_preorder (T root) {
-    assert(root);
+static int subtree_del(BSTreeEntry **root,
+                        BSTreeValue value,
+                        BSTreeCompareFunc cmp)
+{
+    /*
+     * 不存在值为value的节点
+     */
+    if (!*root) {
+        return 0;
+    }
 
-    printf("%d,", root->b);
-    bst_preorder(root->left);
-    bst_preorder(root->right);
-
+    if (cmp(value, (*root)->data) == 0) {
+        /*
+         * 节点匹配，删除之
+         */
+        delete_node(root);
+        return 1;
+        /*
+         * 查找左子树
+         */
+    } else if (cmp(value, (*root)->data) < 0) {
+        subtree_del(&(*root)->left, value, cmp);
+        /*
+         * 查找右子树
+         */
+    } else {
+        subtree_del(&(*root)->right, value, cmp);
+    }
 }
 
-/* 二叉树的中序遍历，标准输出结果 */
-void bst_inorder (T root) {
-    assert(root);
-
-    bst_inorder(root->left);
-    printf("%d,", root->b);
-    bst_inorder(root->right);
+int bstree_del(BSTree *bstree, void *value)
+{
+    return subtree_del(&bstree->root, value, bstree->cmp);
 }
-
-/* 二叉树的后序遍历，标准输出结果 */
-void bst_postorder (T root) {
-    assert(root);
-
-    bst_postorder(root->left);
-    bst_postorder(root->right);
-    printf("%d,", root->b);
-}
-
-
-
-
-
-
--- bstree.h
+++ bstree.h
@@ -1,54 +1,115 @@
+/**
+ * @file bstree.h
+ *
+ * @brief   二叉排序树
+ *
+ * 二叉排序树或者是一棵空树，或者是具有下列性质的二叉树：
+ * （1）若左子树不空，则左子树上所有结点的值均小于或等于它的根结点的值；
+ * （2）若右子树不空，则右子树上所有结点的值均大于或等于它的根结点的值；
+ * （3）左、右子树也分别为二叉排序树
+ *
+ * 使用bstree_new创建新的二叉排序树，
+ * 使用bstree_free销毁已存在的二叉排序树。
+ *
+ * 使用bstree_add插入节点，使用bstree_fin查找已存在的树中是否存在节点，
+ * 使用bstree_del删除节点。
+ *
+ * 使用bstree_depth获取二叉排序树的最大深度。
+ *
+ * 使用bstree_is_empty判断二叉排序树是否为空。
+ */
+
 #ifndef BULLET_BSTREE_H
 #define BULLET_BSTREE_H
 
-#include <stdlib.h>
-#include <stdio.h>
-#define T BSTree_T
-
-typedef struct T *T;
-
-/* 新建二叉树根结点并初始化 */
-extern T bst_new (void);
-
-/* 判断二叉树是否为空 */
-extern int bst_empty (T bst);
-
-/* 求二叉树最大深度 */
-extern int bt_depth (T bst);
-
-/* 释放二叉树 */
-extern void bst_free (T *bst);
-
-/* 二叉树的前序遍历，标准输出结果 */
-extern void bst_preorder (T bst);
-
-/* 二叉树的中序遍历，标准输出结果*/
-extern void bst_inorder (T bst);
-
-/* 二叉树的后序遍历，标准输出结果*/
-extern void bst_postorder (T bst);
-
-/* 比较大小的回调函数 */
-
-/*
- * int (*sort)(void *key1, void *key2)
- * 是用户定义的回调函数。
- * 当key1 > key2 时，返回1，
- * 相等返回0，
- * 小于则返回-1。
+/**
+ * 二叉排序树
  */
+typedef struct _BSTree BSTree;
 
-/* 查找结点 */
-extern int bst_key_search (T bst, void *key,
-                           int (*sort)(void *key1, void *key2));
+/**
+ * 节点数据类型
+ */
+typedef void *BSTreeValue;
 
-/* 插入结点 */
-extern void bst_key_insert (T bst, void *key,
-                            int (*sort)(void *key1, void *key2));
+/**
+ * 定义二叉排序树空指针
+ */
+#define BSTREE_NULL ((void *) 0)
 
-/* 删除结点 */
-extern void bst_key_delete (T bst, void *key,
-                            int (*sort)(void *key1, void *key2));
+/**
+ * BSTreeCompareFunc    回调函数，用于节点数据元素比较大小
+ *
+ * @param value1        元素1
+ * @param value2        元素2
+ * @return              若value1  < value2，则返回值小于0，
+ *                      若value1 == value2，则返回值等于0，
+ *                      若value1  > value2，则返回值大于0。
+ */
+typedef int (*BSTreeCompareFunc)(BSTreeValue value1, BSTreeValue value2);
 
-#undef T
+/**
+ * bstree_new           创建二叉排序树
+ *
+ * @param cmp           节点元素比较函数
+ * @return              创建二叉排序树成功，返回非0值，
+ *                      失败，则返回0。
+ */
+extern BSTree *bstree_new(BSTreeCompareFunc cmp);
+
+/**
+ * bstree_free          销毁二叉排序树
+ *
+ * @param bstree        待销毁的二叉排序树
+ */
+extern void bstree_free(BSTree *bstree);
+
+/**
+ * bstee_add            插入节点
+ *
+ * @param bstree        二叉排序树
+ * @param value         待插入的元素值
+ * @return              插入成功，则返回非0值，
+ *                      失败（内存分配失败），则返回0。
+ */
+extern int bstree_add(BSTree *bstree, BSTreeValue value);
+
+/**
+ * bstree_find          查找节点
+ *
+ * @param bstree        二叉排序树
+ * @param value         待查找的元素值
+ * @return              若二叉排序树中存在节点值为value，则返回非0值，
+ *                      若不存在，则返回0。
+ */
+extern int bstree_find(BSTree *bstree, BSTreeValue value);
+
+/**
+ * bstree_del           删除节点
+ *
+ * @param bstree        二叉排序树
+ * @param value         待删除的节点元素值
+ * @return              若删除成功，则返回非0值，
+ *                      否则，二叉树中不存在该节点，返回0。
+ */
+extern int bstree_del(BSTree *bstree, BSTreeValue value);
+
+/**
+ * bstree_depth         二叉树的最大深度
+ *
+ * @param bstree        二叉排序树
+ * @return              返回二叉排序树的深度，
+ *                      树为空则返回0。
+ */
+extern int bstree_depth(BSTree *bstree);
+
+/**
+ * bstree_is_empty      判断二叉树是否为空
+ *
+ * @param bstree        二叉排序树
+ * @return              若二叉排序树不空，则返回0，
+ *                      若二叉排序树为空，则返回非0值。
+ */
+extern int bstree_is_empty(BSTree *bstree);
+
 #endif /* BULLET_BSTREE_H */
