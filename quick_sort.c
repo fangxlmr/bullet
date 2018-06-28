b5d8df75134cf70d8c2e1715388d653d703d8a91
fangxlmr <fangxlmr@foxmail.com>
2018-06-28 17:15:24 +0800

    another version of qsort

--- quick_sort.c
+++ quick_sort.c
@@ -1,31 +1,76 @@
-int a[101],n;//定义全局变量，这两个变量需要在子函数中使用
-void quick_sort(int left,int right){
+#include <stdio.h>
 
-int i,j,t,pivot;
-if(left > right)
-   return;
-                                
-pivot = a[left]; //pivot中存的就是基准数
-i = left;
-j = right;
-while(i != j){
-    //顺序很重要，要先从右边开始找
-    while(a[j] >= pivot && i < j)
-        j--;
-    //再找左边的
-    while(a[i] <= pivot && i < j)
-        i++;
-    //交换两个数在数组中的位置
-    if(i < j){
-        t = a[i];
-        a[i] = a[j];
-        a[j] = t;
+void qsort(int *, int, int);
+int main (void) {
+    int a[] = {5, 4, 3, 2, 1};
+    int len = sizeof(a) / sizeof(int);
+
+    qsort(a, 0, len - 1);
+    for (int i = 0; i < len; ++i) {
+        printf("%d, ", a[i]);
     }
+    return 0;
 }
-//最终将基准数归位
-a[left] = a[i];
-a[i] = pivot;
 
-quick_sort(left, i - 1);//继续处理左边的，这里是一个递归的过程
-quick_sort(i + 1, right);//继续处理右边的 ，这里是一个递归的过程
+/*
+void quick_sort(int *v, int left, int right)
+{
+    int i, last;
+    void swap(int *v, int i, int j);
+
+    if (left >= right) {
+        return;
+    }
+    swap(v, left, (left + right)/2);
+    last = left;
+    for (i = left + 1; i <= right; ++i) {
+        if (v[i] < v[left]) {
+            swap(v, ++last, i);
+        }
+    }
+    swap(v, left, last);
+    quick_sort(v, left, last - 1);
+    quick_sort(v, last + 1, right);
+}
+
+
+void swap(int *v, int i, int j)
+{
+    int temp;
+
+    temp = v[i];
+    v[i] = v[j];
+    v[j] = temp;
+}
+*/
+
+void qsort(int *nums, int left,int right){
+    int i,j,pivot;
+
+    if(left > right)
+       return;
+
+    pivot = nums[left]; //pivot中存的就是基准数
+    i = left;
+    j = right;
+    while(i != j){
+        //顺序很重要，要先从右边开始找
+        while(nums[j] >= pivot && i < j)
+            j--;
+        //再找左边的
+        while(nums[i] <= pivot && i < j)
+            i++;
+        //交换两个数在数组中的位置
+        if(i < j){
+            int tmp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = tmp;
+        }
+    }
+    //最终将基准数归位
+    nums[left] = nums[i];
+    nums[i] = pivot;
+
+    qsort(nums, left,  i - 1);
+    qsort(nums, i + 1, right);
 }
