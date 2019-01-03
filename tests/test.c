b8789c966aa5d00c211fe512a372d74ba53cc9a1
fangxlmr <fangxlmr@foxmail.com>
2019-01-03 16:03:49 +0800

    [test] Remove printf lines.

--- tests/test.c
+++ tests/test.c
@@ -3,13 +3,22 @@
 #include "vector.h"
 #include "stack.h"
 #include "queue.h"
+#include "dict.h"
 
 static int a[] = {
     11, 23, 35, 20, 
     2, -10, 330, -501,
     0, 0, 25, 78
 };
-int LEN = sizeof(a) / sizeof(a[0]);
+
+static int b[] = {
+    29, 30, 50, 0,
+    44, 600, -2, -90,
+    5, -999, 60, 1000,
+};
+
+int LEN_A = sizeof(a) / sizeof(a[0]);
+int LEN_B = sizeof(b) / sizeof(b[0]);
 
 TEST(vector, testing_vector)
 {
@@ -18,16 +27,16 @@ TEST(vector, testing_vector)
     vector_t vector;
 
     ASSERT_EQ(0, vector_new(&vector));
-    for (i = 0; i < LEN; i++) {
+    for (i = 0; i < LEN_A; i++) {
         EXPECT_EQ(0, vector_append(vector, &a[i]));
     }
 
     EXPECT_EQ(0, vector_append(vector, &a[2]));
     EXPECT_EQ(0, vector_pop(vector, &x));
 
-    EXPECT_EQ(LEN, vector_get_size(vector));
+    EXPECT_EQ(LEN_A, vector_get_size(vector));
 
-    for (i = 0; i < LEN; i++) {
+    for (i = 0; i < LEN_A; i++) {
         EXPECT_EQ(0, vector_get(vector, i, &x));
         EXPECT_EQ(a[i], *(int *)x);
     }
@@ -38,7 +47,7 @@ TEST(vector, testing_vector)
 
     EXPECT_FALSE(vector_isempty(vector));
 
-    for (i = 0; i < LEN; i++) {
+    for (i = 0; i < LEN_A; i++) {
         EXPECT_EQ(0, vector_pop(vector, &x));
     }
 
@@ -52,7 +61,7 @@ TEST(stack, stack_testing) {
     _stack_t stack;
 
     ASSERT_EQ(0, stack_new(&stack));
-    for (i = 0; i < LEN; i++) {
+    for (i = 0; i < LEN_A; i++) {
         EXPECT_EQ(0, stack_push(stack, &a[i]));
     }
 
@@ -61,10 +70,10 @@ TEST(stack, stack_testing) {
     EXPECT_EQ(0, stack_peek(stack, &x));
     EXPECT_EQ(a[2], *(int *) x);
 
-    EXPECT_EQ(LEN, stack_get_size(stack));
+    EXPECT_EQ(LEN_A, stack_get_size(stack));
     EXPECT_FALSE(stack_isempty(stack));
 
-    for (i = 0; i < LEN; i++) {
+    for (i = 0; i < LEN_A; i++) {
         EXPECT_EQ(0, stack_pop(stack, &x));
     }
 
@@ -80,7 +89,7 @@ TEST(queue, queue_testing) {
     queue_t queue;
 
     ASSERT_EQ(0, queue_new(&queue));
-    for (i = 0; i < LEN; i++) {
+    for (i = 0; i < LEN_A; i++) {
         EXPECT_EQ(0, queue_push(queue, &a[i]));
     }
 
@@ -89,10 +98,10 @@ TEST(queue, queue_testing) {
     EXPECT_EQ(0, queue_peek(queue, &x));
     EXPECT_EQ(a[1], *(int *) x);
 
-    EXPECT_EQ(LEN, queue_get_size(queue));
+    EXPECT_EQ(LEN_A, queue_get_size(queue));
     EXPECT_FALSE(queue_isempty(queue));
 
-    for (i = 0; i < LEN; i++) {
+    for (i = 0; i < LEN_A; i++) {
         EXPECT_EQ(0, queue_pop(queue, &x));
     }
 
@@ -101,6 +110,31 @@ TEST(queue, queue_testing) {
     EXPECT_TRUE(queue_isempty(queue));
     queue_free(&queue);
 }
+
+TEST(dict, dict_testing) {
+    int i;
+    dictKey x;
+    dictValue y;
+    dict_t dict;
+
+    ASSERT_EQ(0, dict_new(&dict, NULL, NULL));
+    for (i = 0; i < LEN_A; i++) {
+        EXPECT_EQ(0, dict_add(dict, &a[i], &b[i]));
+    }
+    EXPECT_TRUE(dict_contains_key(dict, &a[0]));
+    EXPECT_EQ(0, dict_remove(dict, &a[0]));
+    EXPECT_FALSE(dict_contains_key(dict, &a[0]));
+
+    EXPECT_EQ(a[1], *(int *) x);
+
+    for (i = 1; i < LEN_A; i++) {
+        EXPECT_EQ(0, dict_get_value(dict, &a[i], &y));
+        EXPECT_EQ(*(int *) y, b[i]);
+    }
+
+    dict_free(&dict);
+}
+
 int main (int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
