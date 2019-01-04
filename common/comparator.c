fe080d4671b53daf2f086c69e10e39b50bde2505
fangxlmr <fangxlmr@foxmail.com>
2019-01-04 16:44:12 +0800

    [comparator] Refacor code

--- common/comparator.c
+++ common/comparator.c
@@ -17,52 +17,20 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
+#include <stdint.h>
 #include "comparator.h"
 
 int cmp_int(const void *x1, const void *x2)
 {
-    int *y1, *y2;
-
-    y1 = (int *) x1;
-    y2 = (int *) x2;
-
-    if (y1 < y2) {
-        return -1;
-    } else if (y1 == y2) {
-        return 0;
-    } else {
-        return 1;
-    }
+    return *(int *) x1 - *(int *) x2;
 }
 
 int cmp_char(const void *x1, const void *x2)
 {
-    char *c1, *c2;
-
-    c1 = (char *) x1;
-    c2 = (char *) x2;
-
-    if (c1 < c2) {
-        return -1;
-    } else if (c1 == c2) {
-        return 0;
-    } else {
-        return 1;
-    }
+    return *(char *) x1 - *(char *) x2;
 }
 
 int cmp_pointer(const void *x1, const void *x2)
 {
-    unsigned int *p1, *p2;
-
-    p1 = (unsigned int *) x1;
-    p2 = (unsigned int *) x2;
-
-    if (p1 < p2) {
-        return -1;
-    } else if (p1 == p2) {
-        return 0;
-    } else {
-        return 1;
-    }
+    return (uintptr_t) x1 - (uintptr_t) x2;
 }
